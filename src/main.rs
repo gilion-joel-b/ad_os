27455d5b217b5ca244e4000bde9b91b880dcf526
Joel W. Blomster <joel.blomster@gilion.com>
2024-09-06 08:56:24 +0200

    compiling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+pub extern "efiapi" fn efi_main() {}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
