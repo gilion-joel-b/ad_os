538373cb47fc66e7880d7530c1a809282daccbc8
Joel W. Blomster <joel.blomster@gilion.com>
2024-09-07 08:22:45 +0200

    Added system table

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![no_main]
 
+mod uefi;
+use uefi::{ImageHandle, SystemTable};
+
 #[no_mangle]
-pub extern "efiapi" fn efi_main() {}
+pub extern "efiapi" fn efi_main(image_handle: ImageHandle, system_table: *const SystemTable) {}
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
